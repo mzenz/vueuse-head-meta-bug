d4a391fd308dac705603007234ba61f6ae34b7b5
Martin Z <mzenze@gmail.com>
2022-10-31 21:59:58 -0300

    repo init 🐣

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>Hello Vite</span>
+</template>
+
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+const meta = [
+  { property: 'article:tag', content: 'tag1' },
+  { property: 'article:tag', content: 'tag2' },
+]
+
+console.debug(meta)
+
+useHead({ title: 'Hello Vite', meta })
+</script>
